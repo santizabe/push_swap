6dcf7f5a385efcc3cf4dac9108b92c591e7c3b5a
Santiago Zapata Bedoya <szapata-@c1r1s3.42malaga.com>
2026-01-20 13:16:48 +0100

    [feat] Add ft_atol function

---
+++ ft_atol.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: szapata- <szapata-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/12 18:54:20 by szapata-          #+#    #+#             */
+/*   Updated: 2024/07/12 18:55:58 by szapata-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+long	ft_atol(const char *str)
+{
+	long	res;
+	int		minus;
+
+	res = 0;
+	minus = 1;
+	while ((*str >= 9 && *str <= 13) || *str == ' ')
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			minus = -1;
+		str++;
+	}
+	while (ft_isdigit(*str))
+	{
+		res = (res * 10) + (*str - 48);
+		str++;
+	}
+	return (res * minus);
+}
